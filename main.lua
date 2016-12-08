db256eeff91784377ee7d11cd7312dd2c6ae7ca5
buckle2000 <buckle2000@zoho.com>
2016-12-08 14:48:46 +0800

    Add signal

--- main.lua
+++ main.lua
@@ -1,20 +1,12 @@
 tostring = require("lib/inspect")
 -- GS       = require("lib/hump.gamestate")
-Class    = require("lib/hump.class")
-Signal   = require("lib/hump.signal")
-Timer    = require("lib/hump.timer")
-U        = require("utils")
+Class  = require("lib/hump.class")
+Signal = require("lib/hump.signal")
+Timer  = require("lib/hump.timer")
+Vec    = require("lib/hump.vector")
+U      = require("utils")
 table.seal(_G)
 -- no global variable declaration afterward!!!
-
-local ball = {
-	x = 300,
-	y = 300,
-	vx = 100,
-	vy = 100,
-	r = 10,
-}
-local ww, hh = love.graphics.getDimensions()
 
 function reset()
 
@@ -22,20 +14,16 @@ end
 love.load = reset
 
 function love.draw()
-	love.graphics.print("Helloworld", 300, 400)
-	love.graphics.circle("line", ball.x, ball.y, ball.r)
+
 end
 
 function love.update(dt)
 	Timer.update(dt)
-	ball.x=ball.x+ball.vx*dt
-	ball.y=ball.y+ball.vy*dt
-	if (hh-ball.y) < ball.r or ball.y<ball.r then 
-		ball.vy = -ball.vy
-		ball.vy = ball.vy
-	end
-	if (ww-ball.x) < ball.r or ball.x < ball.r then
-		ball.vx = -ball.vx
-		ball.vx = ball.vx
-	end
+end
+
+function love.keypressed(key)
+	Signal.emit("keypressed", key)
+end
+function love.keyreleased(key)
+	Signal.emit("keyreleased", key)
 end
